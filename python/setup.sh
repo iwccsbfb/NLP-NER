78f6f787710a861625b48bb093ea3ee8805f7afc
iwccsbfb <zengye.wang@hotmail.com>
2016-10-04 11:20:37 -0400

    update

--- python/setup.sh
+++
@@ -1,7 +0,0 @@
-# only install dependencies for news-corpus-builder by firstly install it and then remove it
-# Use local version of news-corpus-builder, that's why uninstall it
-pip install news-corpus-builder
-pip uninstall news-corpus-builder
-
-# might have error in installing below packages, so install it again
-pip install BeautifulSoup, cssselect
