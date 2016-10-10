91d5ad6505f032fd9b40b8cd79e33bee8bf0fda3
iwccsbfb <zengye.wang@hotmail.com>
2016-10-09 21:57:53 -0400

    update

--- R/data.R
+++ R/data.R
@@ -21,16 +21,16 @@ get_bond_data <- function() {
   invisible()
 }
 
-load_tickers <- function(tickers, file = 'data/comp_names.csv') {
-  tickers <- as.list(tickers)
-  tickers$sep <- ','
-  tickers_str <- do.call(paste, tickers)
-  url <- sprintf('http://finance.yahoo.com/d/quotes.csv?s=%s&f=sn', tickers_str)
-  #download.file(url, destfile = 'data/comp_names.csv', mode='a')
+# input: equity_tickers
+load_tickers <- function(equity_tickers) {
+  equity_tickers <- as.list(equity_tickers)
+  equity_tickers$sep <- ','
+  equity_tickers_str <- do.call(paste, equity_tickers)
+  url <- sprintf('http://finance.yahoo.com/d/quotes.csv?s=%s&f=sn', equity_tickers_str)
   download.file(url, destfile = 'tmp.csv')
   data <- read.table(file='tmp.csv', header=FALSE, sep=',', stringsAsFactors=FALSE)
-  data.nonNA <- data[data[2] != 'N/A',]
-  write.table(data.nonNA, file=file, row.names=FALSE, col.names=FALSE, append=TRUE, quote=FALSE, sep=',')
+  if(file.exists('tmp.csv')) file.remove('tmp.csv')
+  colnames(data) <- c('equity_ticker', 'comp_name')
   data
 }
 
@@ -39,34 +39,48 @@ load_tickers <- function(tickers, file = 'data/comp_names.csv') {
 # some bond ticker may not have equity ticker; 
 #TODO: save bond tickers, save tickers that cannot find names 
 get_company_names <- function() {
-  tickers <- unique(get_secmaster()$bonds$ticker)
+  bond_tickers <- unique(get_secmaster()$bonds$ticker)
   
 #  tickers <- tickers[1:100]
+  result <- data.frame(bond_ticker='a', equity_ticker='a', comp_name='a',stringsAsFactors=FALSE)
   len <- 200
   for(idx in seq(1, length(tickers), len)) {
     idx_end <- idx + len - 1
     if(idx_end > length(tickers)) idx_end <- length(tickers)
-    tickers.sub <- tickers[idx:idx_end]
+    bond_tickers.sub <- bond_tickers[idx:idx_end]
     success <- FALSE
     while(!success) {
-      tryCatch({        
-        data <- load_tickers(tickers.sub)
-        tickers.NA <- data[data[2] == 'N/A',1]
-        tickers.new <- sapply(tickers.NA, function(ticker) {
-          if(nchar(ticker) <= 2) {
-            print(sprintf('%s has <= 2 chars.', ticker))
-            return (NULL)
+      tryCatch({
+        # assume bond ticker is equity ticker first
+        data <- load_tickers(bond_tickers.sub)
+        data <- cbind(bond_ticker=bond_tickers.sub, data, stringsAsFactors=FALSE)
+        result <- rbind(result, data[data$comp_name != 'N/A',])
+        bond_tickers.NA <- data$bond_ticker[data$comp_name == 'N/A']
+        
+        equity_tickers <- sapply(bond_tickers.NA, function(bond_ticker) {
+          if(nchar(bond_ticker) <= 2) {
+            result <<- rbind(result, c(bond_ticker,'NA','NA'))
+        #print(sprintf('%s has <= 2 chars.', bond_ticker))
+            return ('')
           } 
-          ticker <- substr(ticker, 1, nchar(ticker)-2)
-          if(ticker %in% tickers) {
-            print(sprintf("Truncated ticker %s is in tickers set, removing it.", ticker))
-            return (NULL)
+          equity_ticker <- substr(bond_ticker, 1, nchar(bond_ticker)-2)
+          if(bond_ticker %in% bond_tickers) {
+        #print(sprintf("Truncated ticker %s is in tickers set, removing it.", equity_ticker))
+            result <<- rbind(result, c(bond_ticker,equity_ticker,'NA'))
+            return ('')
           } 
-          return (ticker)
+          return (equity_ticker)
         })
-        tickers.new <- tickers.new[!is.null(tickers.new)]
-        data <- load_tickers(tickers.new)
-        print(sprintf('%d tickers cannot find data', sum(data[2] == 'N/A')))
+        idx <- equity_tickers != ''
+        if(any(idx)) {
+          #never reach here
+          print('ahhahah')
+          data <- load_tickers(equity_tickers[idx])
+          data <- cbind(bond_tickers.NA[idx], data)
+          result <- rbind(result, data)
+        }
+        # append bond tickers that are 
+#        print(sprintf('%d tickers cannot find data', sum(data$comp_name == 'N/A')))
         success <- TRUE
       }, error = function(err) {
         print(err)
@@ -75,6 +89,9 @@ get_company_names <- function() {
       })
     }
   }
+  file = 'data/comp_names.csv'
+  result <- result[2:nrow(result),]
+  write.table(result, file=file, row.names=FALSE, col.names=TRUE, append=FALSE, quote=FALSE, sep=',')
   invisible()
 }
 
